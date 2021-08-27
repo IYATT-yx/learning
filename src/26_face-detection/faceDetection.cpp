454c4ea72147acf137cdcbdaa433163d46a712c1
IYATT-yx <2514374431@qq.com>
2021-08-27 18:30:16 +0800

    添加版权声明

--- src/26_face-detection/faceDetection.cpp
+++ src/26_face-detection/faceDetection.cpp
@@ -1,9 +1,18 @@
 /**
  * @file faceDetection.cpp
- * @author IYATT-yx 2514374431@qq.com
  * @brief 人脸检测
- * @copyright Copyright (C) 2021 IYATT-yx
- * 基于 AGPL-3.0 许可 <http://www.gnu.org/licenses/>
+ * 
+ * Copyright (C) 2021 IYATT-yx (Zhao Hongfei, 赵洪飞)，2514374431@qq.com
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include <iostream>
 #include <vector>
