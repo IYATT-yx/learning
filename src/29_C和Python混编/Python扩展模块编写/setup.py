c80c83f4598686c24a8bdf3915888d75ec1492b0
IYATT-yx <2514374431@qq.com>
2021-09-23 01:07:07 +0800

    修改代码：适配Windows下扩展构建

--- src/29_C和Python混编/Python扩展模块编写/setup.py
+++ src/29_C和Python混编/Python扩展模块编写/setup.py
@@ -3,6 +3,8 @@
 @file setup.py
 @brief 为 Python 构建 MD5 扩展
 
+已测试 Windows 10 下使用 MSBuild 和 Ubuntu 20.04 下使用 GNU GCC 构建扩展成功 （都是使用 64 位编译器）
+
 Copyright (C) 2021 IYATT-yx (Zhao Hongfei, 赵洪飞)，2514374431@qq.com
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
@@ -16,10 +18,16 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from distutils.core import setup, Extension
+import sys
 
-module1 = Extension('md5',
-                    sources=['md5.c'],
-                    extra_compile_args=['-std=c17'])
+if sys.platform == 'win32':
+       module1 = Extension('md5',
+                            sources=['md5.c'],
+                            extra_compile_args=['/utf-8', '/std:c17'])
+elif sys.platform == 'linux': 
+       module1 = Extension('md5',
+                            sources=['md5.c'],
+                            extra_compile_args=['-std=c17'])
 
 setup (name='MD5',
        version='1.0',
